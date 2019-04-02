f53568b2dcd6387741edc128b8d8c7463a63c9fa
Marko Milosevic <ninja643@gmail.com>
2019-04-02 11:39:15 +0200

    Add pick contact method Add button style so that button text is not always uppercase

--- app/src/main/java/com/example/contactslist/MainActivity.java
+++ app/src/main/java/com/example/contactslist/MainActivity.java
@@ -1,21 +1,20 @@
 package com.example.contactslist;
 
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.provider.AlarmClock;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.AlarmClock;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "CONTACTS";
+    private static final int REQUEST_PICK_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +22,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void setAlarm(View view) {
+        final Calendar calendar = GregorianCalendar.getInstance();
+
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        final int minute = calendar.get(Calendar.MINUTE) + 1;
+
+        final Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, "Programmatically set")
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minute);
+
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void showAlarms(View view) {
+        final Intent intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
     public void pickContact(View view) {
-        Intent intent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
+        final Intent intent = new Intent(Intent.ACTION_PICK)
+                .setType(ContactsContract.Contacts.CONTENT_TYPE);
 
-        Log.i(TAG, "Intent created");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_PICK_CONTACT);
+        }
+    }
 
-        PackageManager packageManager = getPackageManager();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case REQUEST_PICK_CONTACT:
+                if (resultCode == RESULT_OK && data != null) {
+                    final Uri contactUri = data.getData();
+                    displayContact(contactUri);
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
 
-        ComponentName val = intent.resolveActivity(packageManager);
+    }
 
-        Log.i(TAG, val != null ? "Resolved: " + val.toString() : "Could not resolve");
+    private void displayContact(Uri contactUri) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, contactUri);
 
-        if (val != null) {
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
     }
